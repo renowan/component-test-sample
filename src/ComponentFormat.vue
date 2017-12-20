0cad1145ceabff5a1e7a656641b600e485bcb237
温 穎峰 <on@gnavi.co.jp>
2017-12-20 16:42:46 +0900

    コンポーネント追加

---
+++ src/ComponentFormat.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="">
+    Component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'component-format',
+  props: {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ src/SizeBox.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="my-box" :style="myStyle">
+    myWidth:<span class="my-width">{{myWidth}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'size-box',
+  props: {
+    width: {type: Number, default: 0},
+    ratio: {type: Number, default: 1}
+  },
+  computed: {
+    myWidth () {
+      return Math.floor(this.width * this.ratio)
+    },
+    myStyle () {
+      return {
+        width: `${this.myWidth}px`
+      }
+    }
+  },
+  methods: {
+    add (a, b) {
+      return a + b
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.my-box {
+  height: 50px;
+  background-color: red;
+}
+</style>
